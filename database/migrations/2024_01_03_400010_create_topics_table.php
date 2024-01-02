1345fc05244aaaf29089d3cecfbf61c26c344b31
relliv <epsilonelittr@gmail.com>
2024-01-03 02:18:18 +0300

    Add color field to topics table

--- database/migrations/2024_01_03_400010_create_topics_table.php
+++ database/migrations/2024_01_03_400010_create_topics_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
 
             $table->text("name");
+            $table->text("color");
 
             $table->timestamps();
         });
