5f13c3c65166d9e0faf9569feb5fe1d012d47670
relliv <epsilonelittr@gmail.com>
2024-01-03 02:01:34 +0300

    Create loop actions migration

---
+++ database/migrations/2024_01_03_300001_create_loop_actions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loop_actions', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('loop_id');
+
+            $table->string('name');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loop_actions');
+    }
+};
