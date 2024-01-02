875e0d87671874d1b0a9025b2d9b26e24806f34a
relliv <epsilonelittr@gmail.com>
2024-01-03 02:25:31 +0300

    Update seeders

--- database/seeders/DevSeeder.php
+++ database/seeders/DevSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Database\Seeders\Local\Loop;
 use Database\Seeders\Local\Todo;
+use Database\Seeders\Local\Topic;
 use Database\Seeders\Local\User\DefaultUserSeeder;
 use Database\Seeders\Local\User\UserSeeder;
 use Illuminate\Database\Seeder;
@@ -25,6 +26,10 @@ class DevSeeder extends Seeder
             Loop\LoopSeeder::class,
             Loop\LoopActionSeeder::class,
             Loop\LoopDayActionSeeder::class,
+
+            // topic
+            Topic\TopicSeeder::class,
+            Topic\TopicDayNoteSeeder::class,
 
             // todo
             Todo\TodoSeeder::class,
